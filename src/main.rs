d49a5931dbf59fb2e728d56c10ac472ca15134e6
David Hatfield <davidhatfieldjr@gmail.com>
2023-12-07 21:19:31 -0500

    HE HAS RISEN

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugins(WorldInspectorPlugin::default())
         .add_systems(Startup, setup)
+        .add_systems(Update, move_player)
         .run();
 }
 
@@ -28,4 +29,32 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         },
         Player,
     ));
+
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("tower_guy.png"),
+            sprite: Sprite {
+                custom_size: Some(Vec2::new(100., 100.)),
+                ..default()
+            },
+            transform:Transform::from_translation(Vec3::Y*150.0),
+            ..default()
+        },
+        Player,
+    ));
+}
+
+
+
+fn move_player(input: Res<Input<KeyCode>>, time: Res<Time>, mut player_query: Query<&mut Transform, With<Player>>){
+    for mut transform in &mut player_query{
+        let mut x_speed = 0.0; 
+        if input.pressed(KeyCode::D){
+            x_speed = 20.0*time.delta_seconds();
+        }
+        if input.pressed(KeyCode::A){
+            x_speed = -20.0*time.delta_seconds();
+        }
+        transform.translation += Vec3::X*x_speed;
+    }
 }
