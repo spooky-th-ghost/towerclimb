7e27a34497adb9136af3fe99c78cee144419a3f9
David Hatfield <80296081+thelastwhitesquirrel@users.noreply.github.com>
2023-12-07 22:00:05 -0500

    CAM GAL

--- src/main.rs
+++ src/main.rs
@@ -6,16 +6,22 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_plugins(WorldInspectorPlugin::default())
         .add_systems(Startup, setup)
-        .add_systems(Update, move_player)
+        .add_systems(Update, (move_player, move_camera))
         .run();
 }
 
 #[derive(Component)]
 pub struct Player;
 
+#[derive(Component)]
+pub struct MainCamera;
+
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     // Spawn the camera
-    commands.spawn(Camera2dBundle::default());
+    commands.spawn((
+        Camera2dBundle::default(),
+        MainCamera
+));
 
     // Spawn our guy
     commands.spawn((
@@ -40,21 +46,30 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             transform:Transform::from_translation(Vec3::Y*150.0),
             ..default()
         },
-        Player,
     ));
 }
 
 
 
-fn move_player(input: Res<Input<KeyCode>>, time: Res<Time>, mut player_query: Query<&mut Transform, With<Player>>){
-    for mut transform in &mut player_query{
+fn move_player(input: Res<Input<KeyCode>>, time: Res<Time>, mut player_query: Query<(&mut Transform, &mut Sprite), With<Player>>){
+    for (mut transform, mut sprite) in &mut player_query{
         let mut x_speed = 0.0; 
         if input.pressed(KeyCode::D){
-            x_speed = 20.0*time.delta_seconds();
+            x_speed = 100.0*time.delta_seconds();
+            sprite.flip_x = false;
         }
         if input.pressed(KeyCode::A){
-            x_speed = -20.0*time.delta_seconds();
+            x_speed = -100.0*time.delta_seconds();
+            sprite.flip_x = true;
+
         }
         transform.translation += Vec3::X*x_speed;
     }
 }
+
+fn move_camera(mut camera_query: Query<&mut Transform, With<MainCamera>>, player_query: Query<&Transform,(With<Player>, Without<MainCamera>)>){
+    let mut camera_transform = camera_query.single_mut();
+    let player_transform = player_query.single();
+    camera_transform.translation.x = player_transform.translation.x;
+}
+
