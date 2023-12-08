02a6f2c51326bccb3d74a7a34e14033c5e0c5e02
spooky-th-ghost <colin@wildheartinteractive.com>
2023-12-07 20:18:25 -0500

    boilerplate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use bevy::prelude::*;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(WorldInspectorPlugin::default())
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
